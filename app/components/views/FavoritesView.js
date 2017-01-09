bfa3d079a01f2b6fa672946e474f2ad6fcc13be5
ConstaGorgan <consta.gorgan@gmail.com>
2017-01-09 12:39:01 +0200

    Added js & jst defaults

---
+++ app/components/views/FavoritesView.js
@@ -0,0 +1,26 @@
+/* global $ */
+import {Marionette} from '../../../vendor/vendor'
+import template from '../../templates/favorites.jst'
+import HeaderView from './HeaderView'
+import FooterView from './FooterView'
+
+export default Marionette.View.extend({
+  el: '#app',
+  template: template,
+
+  regions: {
+    header: '#header',
+    footer: '#footer'
+  },
+
+  events: {
+
+  },
+
+  onRender: function () {
+    $(window).scrollTop(0)
+
+    this.showChildView('header', new HeaderView())
+    this.showChildView('footer', new FooterView())
+  }
+})
