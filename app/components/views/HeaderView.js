b7e5ac949b89019bd90b68a561cf1cb3fdee591d
ConstaGorgan <consta.gorgan@gmail.com>
2017-01-11 01:48:35 +0200

    Added mobile menu functionality & design - not ready

--- app/components/views/HeaderView.js
+++ app/components/views/HeaderView.js
@@ -8,9 +8,17 @@ export default Marionette.View.extend({
   templateContext: {
     userName: sessionStorage.userName
   },
-
   events: {
-
+    'click .header_btn': 'toggleSideMenu'
+  },
+  toggleSideMenu: function (e) {
+//    if($('.menu_mobile').hasClass('display_inline_block')) {
+      if($('.menu_mobile').hasClass('display_inline_block')) {
+      $(event.target).removeClass('header_btn_close')
+      $('.menu_mobile').removeClass('display_inline_block')
+    } else {
+      $(event.target).addClass('header_btn_close')
+      $('.menu_mobile').addClass('display_inline_block')
+    }
   }
-
 })
